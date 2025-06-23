d677fcd12377fb40bf004b5d68b315644616fbcd
Saketh Goljana <114288715+SakethGoljana@users.noreply.github.com>
2025-06-23 14:33:11 +0530

    yt-search- with-debouncing

---
+++ DAY6/Yotube-Clone/yt-search/search.js
@@ -0,0 +1,42 @@
+const renderSuggestions = (data) => {
+  const { results } = data;
+  const rootElem = document.getElementById("search-suggestions");
+  rootElem.innerText = "";
+  results.forEach((txt) => {
+    const newPara = document.createElement("p");
+    newPara.innerText = txt;
+    rootElem.appendChild(newPara);
+  });
+};
+
+const searchsuggestions = (e) => {
+  const search = e.target.value;
+
+  const request = fetch(
+    `https://youtube138.p.rapidapi.com/auto-complete/?q=${search}&hl=en&gl=US`,
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": "3ed7f7fb35msh1d6eb40e20aac68p1e31d3jsne169b50c3121",
+        "x-rapidapi-host": "youtube138.p.rapidapi.com",
+      },
+    }
+  );
+  request.then((response) => {
+    const pr2 = response.json();
+    pr2
+      .then((data) => {
+        renderSuggestions(data);
+      })
+      .catch((err) => {
+        console.error("Error parsing JSON:", err);
+      });
+  });
+};
+let timeoutId = null;
+const timesearch = (e) => {
+  clearTimeout(timeoutId);
+  timeoutId = setTimeout(() => {
+    searchsuggestions(e);
+  }, 2000);
+};
